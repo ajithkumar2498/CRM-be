ff710287acdaac354256bba81479bb6a6e45890f
ajithkumar2498 <ajibhc24@gmail.com>
2025-05-29 04:57:33 +0530

    url updated

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 8000
 const app = express()
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONT_END_URL,
 }));
 app.use(express.json());
 
